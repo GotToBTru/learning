d8c27c60171608300fb9f12871ad3a8f10226226
Howard Parks <GotToBTru@gmail.com>
2016-07-16 22:31:15 -0500

    Now two files

--- file1.pl
+++ file1.pl
@@ -2,4 +2,6 @@ use strict;
 use warnings;
 my $name = 'file1';
 printf "My name is %20s\n",$name;
+print "Now moving to file 2 ...\n";
+exec('perl','file2.pl');
 
---
+++ file2.pl
@@ -0,0 +1,5 @@
+use strict;
+use warnings;
+
+print "Now in file 2.\n";
+
